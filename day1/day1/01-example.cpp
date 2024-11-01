d3d16f3f258aee117af56542334be7fe50500156
takealook2 <a1vm5h@gmail.com>
2024-11-02 08:48:37 +0900

    day1: 2750

---
+++ day1/day1/01-example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+	int findNumber;
+	srand(time(NULL));
+	findNumber = rand() % 100;
+
+	for (int i = 0; i < 100; i++) {
+		if (i == findNumber) {
+			cout << i;
+			break;
+		}
+	}
+}
